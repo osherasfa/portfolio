cc2e3636ad465eab30db533f386f19613db45f0e
Osher <osherasfa@gmail.com>
2023-10-21 19:50:28 +0300

    V1.0.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import githubLogo from "./assets/github.png"
+import linkedinLogo from "./assets/linkedin.png"
+import projectOne from "./assets/project1.jpg"
+import projectTwo from "./assets/project2.jpg"
+import { TypeAnimation } from 'react-type-animation'
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <a href="."><h1>Osher.Dev</h1></a>
+        <div className="menu">
+          <a href="https://github.com/osherasfa" target="_blank" rel="noreferrer"><img src={githubLogo} alt="github" /></a>
+          <a href="https://www.linkedin.com/in/osher-asefa-b919b020b/" target="_blank" rel="noreferrer"><img src={linkedinLogo} alt="linkedin" /></a>
+          <a className="CV" href="https://docs.google.com/document/d/1rZVyA2B9IYODjbo-Hn1_M-o_6TneyaiqM3_4EFmwrqQ/edit?usp=sharing" target="_blank" rel="noreferrer">CV</a>
+        </div>
+      </nav>
+      <main>
+        <h1 className="intro">Hello, I am Osher :) <br/> A&nbsp;
+          <span className="title">
+            <TypeAnimation
+              sequence={[
+                'Frontend', 
+                1000,
+                'Backend',
+                1000,
+                'Fullstack',
+                1000,
+                'Web',
+                1000
+              ]}
+              wrapper="span"
+              cursor={true}
+              repeat={Infinity}
+            />
+            developer
+          </span>
+        </h1>
+        <section id="aboutme">
+          <h5>- About me -</h5>
+          <p>I am a self-taught programmer with a strong aptitude for technology.<br/>I am curious and explorative, and I enjoy solving unfamiliar problems that make me thing outside of the box.<br/>I am also enjoy reading books 📚, working out 💪, and running 🏃‍️.</p>
+        </section>
+        <section id="projects">
+          <h5>- Projects -</h5>
+          <div className="showcase">
+            <img src={projectOne} alt="projectOne"/>
+            <img src={projectTwo} alt="projectTwo"/>
+          </div>
+        </section>
+        <section id="skills">
+          <h5>- Skills -</h5>
+          <div>
+            <img src="" alt=""/>
+            <img src="" alt=""/>
+            <img src="" alt=""/>
+            <img src="" alt=""/>
+            <img src="" alt=""/>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <h3>COPYRIGHT © 2023 OsherDev <br/> All Rights Reserved</h3>
+      </footer>
     </div>
   );
 }
-
-export default App;
